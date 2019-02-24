e2c83a4e5e7f9d006ee5c79364167de70f97cd45
jocular-markup-2001 <andreacannuni@gmail.com>
2019-02-24 23:09:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function takeANumber(array, name) {
   array.push(name);
   return `Welcome, ${name}. You are number ${array.length} in line.`
 }
+
+function nowServing(array) {
+  array.unshift();
+  return array;
+}
